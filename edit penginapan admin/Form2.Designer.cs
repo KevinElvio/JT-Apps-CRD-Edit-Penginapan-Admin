9b28e0ccddb19c6d6d4a40368258a73f91b66cb9
kevin <kevin@KEVIN>
2023-06-15 20:19:02 +0700

    Add project files.

---
+++ edit penginapan admin/Form2.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace edit_penginapan_admin
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.tb_harga = new System.Windows.Forms.TextBox();
+            this.tb_fasilitas = new System.Windows.Forms.TextBox();
+            this.tb_nomor_kamar = new System.Windows.Forms.TextBox();
+            this.tb_kapasitas = new System.Windows.Forms.TextBox();
+            this.tb_keterangan = new System.Windows.Forms.TextBox();
+            this.tb_judul = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb_foto_penginapan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_foto_penginapan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_harga
+            // 
+            this.tb_harga.Location = new System.Drawing.Point(540, 544);
+            this.tb_harga.Multiline = true;
+            this.tb_harga.Name = "tb_harga";
+            this.tb_harga.Size = new System.Drawing.Size(301, 53);
+            this.tb_harga.TabIndex = 31;
+            // 
+            // tb_fasilitas
+            // 
+            this.tb_fasilitas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_fasilitas.Location = new System.Drawing.Point(541, 333);
+            this.tb_fasilitas.Multiline = true;
+            this.tb_fasilitas.Name = "tb_fasilitas";
+            this.tb_fasilitas.Size = new System.Drawing.Size(301, 176);
+            this.tb_fasilitas.TabIndex = 30;
+            // 
+            // tb_nomor_kamar
+            // 
+            this.tb_nomor_kamar.Location = new System.Drawing.Point(540, 250);
+            this.tb_nomor_kamar.Multiline = true;
+            this.tb_nomor_kamar.Name = "tb_nomor_kamar";
+            this.tb_nomor_kamar.Size = new System.Drawing.Size(301, 48);
+            this.tb_nomor_kamar.TabIndex = 29;
+            // 
+            // tb_kapasitas
+            // 
+            this.tb_kapasitas.Location = new System.Drawing.Point(540, 180);
+            this.tb_kapasitas.Multiline = true;
+            this.tb_kapasitas.Name = "tb_kapasitas";
+            this.tb_kapasitas.Size = new System.Drawing.Size(301, 38);
+            this.tb_kapasitas.TabIndex = 28;
+            // 
+            // tb_keterangan
+            // 
+            this.tb_keterangan.Location = new System.Drawing.Point(18, 250);
+            this.tb_keterangan.Multiline = true;
+            this.tb_keterangan.Name = "tb_keterangan";
+            this.tb_keterangan.Size = new System.Drawing.Size(391, 409);
+            this.tb_keterangan.TabIndex = 27;
+            // 
+            // tb_judul
+            // 
+            this.tb_judul.Location = new System.Drawing.Point(18, 180);
+            this.tb_judul.Multiline = true;
+            this.tb_judul.Name = "tb_judul";
+            this.tb_judul.Size = new System.Drawing.Size(391, 38);
+            this.tb_judul.TabIndex = 26;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(722, 616);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 43);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Simpan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(584, 616);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 43);
+            this.button1.TabIndex = 24;
+            this.button1.Text = " Hapus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(537, 525);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 16);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Harga";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(538, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 16);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Fasilitas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(537, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 16);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Nomor Kamar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Keterangan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Kapasitas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Judul";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Foto";
+            // 
+            // pb_foto_penginapan
+            // 
+            this.pb_foto_penginapan.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pb_foto_penginapan.BackgroundImage")));
+            this.pb_foto_penginapan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_foto_penginapan.Location = new System.Drawing.Point(18, 44);
+            this.pb_foto_penginapan.Name = "pb_foto_penginapan";
+            this.pb_foto_penginapan.Size = new System.Drawing.Size(823, 99);
+            this.pb_foto_penginapan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_foto_penginapan.TabIndex = 16;
+            this.pb_foto_penginapan.TabStop = false;
+            this.pb_foto_penginapan.Click += new System.EventHandler(this.pb_foto_penginapan_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 770);
+            this.Controls.Add(this.tb_harga);
+            this.Controls.Add(this.tb_fasilitas);
+            this.Controls.Add(this.tb_nomor_kamar);
+            this.Controls.Add(this.tb_kapasitas);
+            this.Controls.Add(this.tb_keterangan);
+            this.Controls.Add(this.tb_judul);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb_foto_penginapan);
+            this.Name = "Form2";
+            this.Text = "Hapus ";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_foto_penginapan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_harga;
+        private System.Windows.Forms.TextBox tb_fasilitas;
+        private System.Windows.Forms.TextBox tb_nomor_kamar;
+        private System.Windows.Forms.TextBox tb_kapasitas;
+        private System.Windows.Forms.TextBox tb_keterangan;
+        private System.Windows.Forms.TextBox tb_judul;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pb_foto_penginapan;
+    }
+}
---
+++ edit penginapan admin/Form2.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace edit_penginapan_admin
+{
+    public partial class Form2 : Form
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=Pariwisata;User Id=postgres;Password=timotius;");
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        string imglocation = "";
+        SqlCommand cmd;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void pb_foto_penginapan_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "png files(*.png)|*.png|jpg files(*.jpg)|*.jpg|All files(*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imglocation = dialog.FileName.ToString();
+                pb_foto_penginapan.ImageLocation = imglocation;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            byte[] images = null;
+            
+
+            FileStream stream = new FileStream(imglocation, FileMode.Open, FileAccess.Read);
+            BinaryReader brs = new BinaryReader(stream);
+            images = brs.ReadBytes((int)stream.Length);
+
+            conn.Open();
+            NpgsqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into edit_penginapan (judul, keterangan, kapasitas, nomor_kamar, fasilitas, harga, foto) values ('" + tb_judul.Text + "','" + tb_keterangan.Text + "','" + tb_kapasitas.Text + "','" + tb_nomor_kamar.Text + "','" + tb_fasilitas.Text + "','" + tb_harga.Text + "',@images) ";
+            cmd.Parameters.Add(new NpgsqlParameter("@images", images));
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+
+            if (tb_judul.Text == "")
+            {
+                MessageBox.Show("Masukan Judul Terlebih Dahulu");
+
+            }
+            else if (tb_keterangan.Text == "")
+            {
+                MessageBox.Show("Masukan Keterangan Terlebih Dahulu");
+            }
+            else if (tb_kapasitas.Text == "")
+            {
+                MessageBox.Show("Masukan Kapasitas Terlebih Dahulu");
+            }
+            else if (tb_nomor_kamar.Text == "")
+            {
+                MessageBox.Show("Masukan Nomor Kamar Terlebih Dahulu");
+            }
+            else if (tb_fasilitas.Text == "")
+            {
+                MessageBox.Show("Masukan Fasilitas Terlebih Dahulu");
+            }
+            else if (tb_harga.Text == "")
+            {
+                MessageBox.Show("Masukan Harga Terlebih Dahulu");
+            }
+            else
+            {
+                //cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Berhasil Disimpan");
+                tb_judul.Text = "";
+                tb_keterangan.Text = "";
+                tb_kapasitas.Text = "";
+                tb_nomor_kamar.Text = "";
+                tb_fasilitas.Text = "";
+                tb_harga.Text = "";
+            }
+            
+
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from edit_penginapan where judul = '" + tb_judul.Text + "', keterangan = '"+tb_keterangan.Text+"', kapasitas = '"+tb_kapasitas.Text+"',nomor_kamar = '"+tb_nomor_kamar.Text+"', fasilitas = '"+tb_fasilitas.Text+"', harga = '"+tb_harga.Text+"', foto = @images ";
+            //cmd.ExecuteNonQuery();
+            //koneksi.Close();
+            //tb_hapus_idstaf.Text = "";
+
+            if (tb_judul.Text == "")
+            {
+                MessageBox.Show("Masukan Judul Terlebih Dahulu");
+
+            }
+            else if (tb_keterangan.Text == "")
+            {
+                MessageBox.Show("Masukan Keterangan Terlebih Dahulu");
+            }
+            else if (tb_kapasitas.Text == "")
+            {
+                MessageBox.Show("Masukan Kapasitas Terlebih Dahulu");
+            }
+            else if (tb_nomor_kamar.Text == "")
+            {
+                MessageBox.Show("Masukan Nomor Kamar Terlebih Dahulu");
+            }
+            else if (tb_fasilitas.Text == "")
+            {
+                MessageBox.Show("Masukan Fasilitas Terlebih Dahulu");
+            }
+            else if (tb_harga.Text == "")
+            {
+                MessageBox.Show("Masukan Harga Terlebih Dahulu");
+            }
+            else
+            {
+                cmd.ExecuteNonQuery(); 
+                MessageBox.Show("Data Berhasil Dihapus");
+                this.Close();
+            }
+            conn.Close();
+        }
+    }
+}
